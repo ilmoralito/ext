203d9dc4c0fb1c13fa9c2fbea5985c874f49caca
Mario Martinez <amakenadog@gmail.com>
2015-11-06 16:02:25 -0600

    Improved department domain class

--- grails-app/domain/ni/edu/uccleon/Department.groovy
+++ grails-app/domain/ni/edu/uccleon/Department.groovy
@@ -1,6 +1,11 @@
 package ni.edu.uccleon
 
+import org.grails.databinding.BindUsing
+
 class Department {
+  @BindUsing({
+    obj, source -> source["name"]?.toLowerCase()?.tokenize(" ")*.capitalize().join(" ")
+  })
   String name
   Integer extensionNumber
 
